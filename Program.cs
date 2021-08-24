23be6813758c43a974224adec04f69df5dbbdda8
Aaron Robinson <aaronrobin1234@gmail.com>
2021-08-24 18:28:07 -0500

    Fix session auth flag

--- Program.cs
+++ Program.cs
@@ -45,8 +45,8 @@ namespace ThunderstoreCLI
         [Option("repository", Required = false, HelpText = "URL of the repository where to publish.")]
         public string Repository { get; set; }
         
-        [Option("use-session-auth", Required = false, HelpText = "Use session auth instead of bearer auth. !!THIS WILL BE DEPRECATED!!")]
-        public bool? UseSessionAuth { get; set; }
+        [Option("use-session-auth", Default = false, Required = false, HelpText = "Use session auth instead of bearer auth. !!THIS WILL BE DEPRECATED!!")]
+        public bool UseSessionAuth { get; set; }
     }
 
     class Program
