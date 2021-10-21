47eef9723683edbac9aac3542825c9f6cacc9a97
Antti Mäki <antti@anttimaki.com>
2021-10-21 13:14:32 +0300

    DRY up ParameterConfig by defining common methods in base class

--- Config/CLIParameterConfig.cs
+++ Config/CLIParameterConfig.cs
@@ -3,7 +3,7 @@ using static Crayon.Output;
 
 namespace ThunderstoreCLI.Config
 {
-    public abstract class CLIParameterConfig<T> : EmptyConfig
+    public abstract class CLIParameterConfig<T> : EmptyConfig where T : PackageOptions
     {
         protected T options;
 
@@ -11,78 +11,40 @@ namespace ThunderstoreCLI.Config
         {
             this.options = options;
         }
+
+        public override GeneralConfig GetGeneralConfig()
+        {
+            return new GeneralConfig()
+            {
+                ProjectConfigPath = options.ConfigPath
+            };
+        }
+
+        public override PackageMeta GetPackageMeta()
+        {
+            if (options == null) return null;
+            return new PackageMeta()
+            {
+                Namespace = options.Namespace,
+                Name = options.Name,
+                VersionNumber = options.VersionNumber
+            };
+        }
     }
 
     public class CLIInitCommandConfig : CLIParameterConfig<InitOptions>
     {
         public CLIInitCommandConfig(InitOptions options) : base(options) { }
-
-        public override GeneralConfig GetGeneralConfig()
-        {
-            return new GeneralConfig()
-            {
-                ProjectConfigPath = options.ConfigPath
-            };
-        }
-
-        public override PackageMeta GetPackageMeta()
-        {
-            if (options == null) return null;
-            return new PackageMeta()
-            {
-                Namespace = options.Namespace,
-                Name = options.Name,
-                VersionNumber = options.VersionNumber
-            };
-        }
     }
 
     public class CLIBuildCommandConfig : CLIParameterConfig<BuildOptions>
     {
         public CLIBuildCommandConfig(BuildOptions options) : base(options) { }
-
-        public override GeneralConfig GetGeneralConfig()
-        {
-            return new GeneralConfig()
-            {
-                ProjectConfigPath = options.ConfigPath
-            };
-        }
-
-        public override PackageMeta GetPackageMeta()
-        {
-            if (options == null) return null;
-            return new PackageMeta()
-            {
-                Namespace = options.Namespace,
-                Name = options.Name,
-                VersionNumber = options.VersionNumber
-            };
-        }
     }
 
     public class CLIPublishCommandConfig : CLIParameterConfig<PublishOptions>
     {
         public CLIPublishCommandConfig(PublishOptions options) : base(options) { }
-
-        public override GeneralConfig GetGeneralConfig()
-        {
-            return new GeneralConfig()
-            {
-                ProjectConfigPath = options.ConfigPath
-            };
-        }
-
-        public override PackageMeta GetPackageMeta()
-        {
-            if (options == null) return null;
-            return new PackageMeta()
-            {
-                Namespace = options.Namespace,
-                Name = options.Name,
-                VersionNumber = options.VersionNumber
-            };
-        }
 
         public override PublishConfig GetPublishConfig()
         {
