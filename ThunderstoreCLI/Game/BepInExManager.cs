8fdb18a216a7379863d1db31ff9d910f63aade7c
Aaron Robinson <aaronrobin1234@gmail.com>
2022-01-31 22:14:20 -0600

    Add BepInEx loader install logic

--- ThunderstoreCLI/Game/BepInExManager.cs
+++ ThunderstoreCLI/Game/BepInExManager.cs
@@ -8,7 +8,23 @@ public class BepInExManager : ModManager
     public bool SupportsProfiles(GameDefinition gameDef) => true;
     public void InstallLoader(GameDefinition gameDef, ModProfile profile, PackageManifestV1 manifest, ZipArchive loaderArchive)
     {
-        throw new NotImplementedException();
+        var winhttp = loaderArchive.Entries.First(x => x.Name == "winhttp.dll");
+        var doorstop_config = loaderArchive.Entries.First(x => x.Name == "doorstop_config.ini");
+
+        var gameDir = gameDef.InstallDirectory;
+
+        winhttp.ExtractToFile(Path.Combine(gameDir, "winhttp.dll"), true);
+        doorstop_config.ExtractToFile(Path.Combine(gameDir, "doorstop_config.ini"));
+
+        var bepFiles = loaderArchive.Entries
+            .Where(x => x != winhttp && x != doorstop_config)
+            .Where(x => Path.GetPathRoot(x.FullName)?.StartsWith("BepInExPack") ?? false);
+
+        foreach (var file in bepFiles)
+        {
+            var actualPath = Path.GetRelativePath(Path.GetPathRoot(file.FullName)!, file.FullName);
+            file.ExtractToFile(Path.Combine(profile.ProfileDirectory, actualPath));
+        }
     }
     public void UninstallLoader(GameDefinition gameDef, ModProfile profile)
     {
