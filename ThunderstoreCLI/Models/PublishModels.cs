88f74190704656a7c2271c0db73815c7009b65ef
Aaron Robinson <aaronrobin1234@gmail.com>
2022-03-13 22:45:39 -0500

    Switch JSON (de)serialization to Newtonsoft.Json

--- ThunderstoreCLI/Models/PublishModels.cs
+++ ThunderstoreCLI/Models/PublishModels.cs
@@ -1,202 +1,128 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace ThunderstoreCLI.Models.Publish;
 
-[ExcludeFromCodeCoverage]
-public class PackageUploadMetadata : BaseJson<PackageUploadMetadata, PackageUploadMetadataContext>
+public class PackageUploadMetadata : BaseJson<PackageUploadMetadata>
 {
-    [JsonPropertyName("author_name")]
-    public string? AuthorName { get; set; }
+    [JsonProperty("author_name")] public string? AuthorName { get; set; }
 
-    [JsonPropertyName("categories")]
-    public string[]? Categories { get; set; }
+    [JsonProperty("categories")] public string[]? Categories { get; set; }
 
-    [JsonPropertyName("communities")]
-    public string[]? Communities { get; set; }
+    [JsonProperty("communities")] public string[]? Communities { get; set; }
 
-    [JsonPropertyName("has_nsfw_content")]
-    public bool HasNsfwContent { get; set; }
+    [JsonProperty("has_nsfw_content")] public bool HasNsfwContent { get; set; }
 
-    [JsonPropertyName("upload_uuid")]
-    public string? UploadUUID { get; set; }
+    [JsonProperty("upload_uuid")] public string? UploadUUID { get; set; }
 }
 
-[JsonSerializable(typeof(PackageUploadMetadata))]
-[ExcludeFromCodeCoverage]
-public partial class PackageUploadMetadataContext : JsonSerializerContext { }
-
-[ExcludeFromCodeCoverage]
-public class UploadInitiateData : BaseJson<UploadInitiateData, UploadInitiateDataContext>
+public class UploadInitiateData : BaseJson<UploadInitiateData>
 {
     public class UserMediaData
     {
-        [JsonPropertyName("uuid")]
-        public string? UUID { get; set; }
+        [JsonProperty("uuid")] public string? UUID { get; set; }
 
-        [JsonPropertyName("filename")]
-        public string? Filename { get; set; }
+        [JsonProperty("filename")] public string? Filename { get; set; }
 
-        [JsonPropertyName("size")]
-        public long Size { get; set; }
+        [JsonProperty("size")] public long Size { get; set; }
 
-        [JsonPropertyName("datetime_created")]
-        public DateTime TimeCreated { get; set; }
+        [JsonProperty("datetime_created")] public DateTime TimeCreated { get; set; }
 
-        [JsonPropertyName("expiry")]
-        public DateTime? ExpireTime { get; set; }
+        [JsonProperty("expiry")] public DateTime? ExpireTime { get; set; }
 
-        [JsonPropertyName("status")]
-        public string? Status { get; set; }
+        [JsonProperty("status")] public string? Status { get; set; }
     }
 
     public class UploadPartData
     {
-        [JsonPropertyName("part_number")]
-        public int PartNumber { get; set; }
+        [JsonProperty("part_number")] public int PartNumber { get; set; }
 
-        [JsonPropertyName("url")]
-        public string? Url { get; set; }
+        [JsonProperty("url")] public string? Url { get; set; }
 
-        [JsonPropertyName("offset")]
-        public long Offset { get; set; }
+        [JsonProperty("offset")] public long Offset { get; set; }
 
-        [JsonPropertyName("length")]
-        public int Length { get; set; }
+        [JsonProperty("length")] public int Length { get; set; }
     }
 
-    [JsonPropertyName("user_media")]
-    public UserMediaData? Metadata { get; set; }
+    [JsonProperty("user_media")] public UserMediaData? Metadata { get; set; }
 
-    [JsonPropertyName("upload_urls")]
-    public UploadPartData[]? UploadUrls { get; set; }
+    [JsonProperty("upload_urls")] public UploadPartData[]? UploadUrls { get; set; }
 }
 
-[JsonSerializable(typeof(UploadInitiateData))]
-[ExcludeFromCodeCoverage]
-public partial class UploadInitiateDataContext : JsonSerializerContext { }
-
-[ExcludeFromCodeCoverage]
-public class FileData : BaseJson<FileData, FileDataContext>
+public class FileData : BaseJson<FileData>
 {
-    [JsonPropertyName("filename")]
-    public string? Filename { get; set; }
+    [JsonProperty("filename")] public string? Filename { get; set; }
 
-    [JsonPropertyName("file_size_bytes")]
-    public long Filesize { get; set; }
+    [JsonProperty("file_size_bytes")] public long Filesize { get; set; }
 }
 
-[JsonSerializable(typeof(FileData))]
-[ExcludeFromCodeCoverage]
-public partial class FileDataContext : JsonSerializerContext { }
-
-[ExcludeFromCodeCoverage]
-public class CompletedUpload : BaseJson<CompletedUpload, CompletedUploadContext>
+public class CompletedUpload : BaseJson<CompletedUpload>
 {
     public class CompletedPartData
     {
-        [JsonPropertyName("ETag")]
-        public string? ETag { get; set; }
+        [JsonProperty("ETag")] public string? ETag { get; set; }
 
-        [JsonPropertyName("PartNumber")]
-        public int PartNumber { get; set; }
+        [JsonProperty("PartNumber")] public int PartNumber { get; set; }
     }
 
-    [JsonPropertyName("parts")]
-    public CompletedPartData[]? Parts { get; set; }
+    [JsonProperty("parts")] public CompletedPartData[]? Parts { get; set; }
 }
 
-[JsonSerializable(typeof(CompletedUpload))]
-public partial class CompletedUploadContext : JsonSerializerContext { }
-
 // JSON response structure for publish package request.
-[ExcludeFromCodeCoverage]
-public class PublishData : BaseJson<PublishData, PublishDataContext>
+public class PublishData : BaseJson<PublishData>
 {
     public class AvailableCommunityData
     {
         public class CommunityData
         {
-            [JsonPropertyName("identifier")]
-            public string? Identifier { get; set; }
+            [JsonProperty("identifier")] public string? Identifier { get; set; }
 
-            [JsonPropertyName("name")]
-            public string? Name { get; set; }
+            [JsonProperty("name")] public string? Name { get; set; }
 
-            [JsonPropertyName("discord_url")]
-            public string? DiscordUrl { get; set; }
+            [JsonProperty("discord_url")] public string? DiscordUrl { get; set; }
 
-            [JsonPropertyName("wiki_url")]
-            public object? WikiUrl { get; set; }
+            [JsonProperty("wiki_url")] public object? WikiUrl { get; set; }
 
-            [JsonPropertyName("require_package_listing_approval")]
+            [JsonProperty("require_package_listing_approval")]
             public bool RequirePackageListingApproval { get; set; }
         }
 
-        public class CategoryData
-        {
-            [JsonPropertyName("name")]
-            public string? Name { get; set; }
+        [JsonProperty("community")] public CommunityData? Community { get; set; }
 
-            [JsonPropertyName("slug")]
-            public string? Slug { get; set; }
-        }
+        [JsonProperty("categories")] public List<string>? Categories { get; set; }
 
-        [JsonPropertyName("community")]
-        public CommunityData? Community { get; set; }
-
-        [JsonPropertyName("categories")]
-        public List<CategoryData>? Categories { get; set; }
-
-        [JsonPropertyName("url")]
-        public string? Url { get; set; }
+        [JsonProperty("url")] public string? Url { get; set; }
     }
 
     public class PackageVersionData
     {
-        [JsonPropertyName("namespace")]
-        public string? Namespace { get; set; }
+        [JsonProperty("namespace")] public string? Namespace { get; set; }
 
-        [JsonPropertyName("name")]
-        public string? Name { get; set; }
+        [JsonProperty("name")] public string? Name { get; set; }
 
-        [JsonPropertyName("version_number")]
-        public string? VersionNumber { get; set; }
+        [JsonProperty("version_number")] public string? VersionNumber { get; set; }
 
-        [JsonPropertyName("full_name")]
-        public string? FullName { get; set; }
+        [JsonProperty("full_name")] public string? FullName { get; set; }
 
-        [JsonPropertyName("description")]
-        public string? Description { get; set; }
+        [JsonProperty("description")] public string? Description { get; set; }
 
-        [JsonPropertyName("icon")]
-        public string? Icon { get; set; }
+        [JsonProperty("icon")] public string? Icon { get; set; }
 
-        [JsonPropertyName("dependencies")]
-        public List<string>? Dependencies { get; set; }
+        [JsonProperty("dependencies")] public List<string>? Dependencies { get; set; }
 
-        [JsonPropertyName("download_url")]
-        public string? DownloadUrl { get; set; }
+        [JsonProperty("download_url")] public string? DownloadUrl { get; set; }
 
-        [JsonPropertyName("downloads")]
-        public int Downloads { get; set; }
+        [JsonProperty("downloads")] public int Downloads { get; set; }
 
-        [JsonPropertyName("date_created")]
-        public DateTime DateCreated { get; set; }
+        [JsonProperty("date_created")] public DateTime DateCreated { get; set; }
 
-        [JsonPropertyName("website_url")]
-        public string? WebsiteUrl { get; set; }
+        [JsonProperty("website_url")] public string? WebsiteUrl { get; set; }
 
-        [JsonPropertyName("is_active")]
-        public bool IsActive { get; set; }
+        [JsonProperty("is_active")] public bool IsActive { get; set; }
     }
 
-    [JsonPropertyName("available_communities")]
+    [JsonProperty("available_communities")]
     public List<AvailableCommunityData>? AvailableCommunities { get; set; }
 
-    [JsonPropertyName("package_version")]
-    public PackageVersionData? PackageVersion { get; set; }
+    [JsonProperty("package_version")] public PackageVersionData? PackageVersion { get; set; }
 }
-
-[JsonSerializable(typeof(PublishData))]
-public partial class PublishDataContext : JsonSerializerContext { }
