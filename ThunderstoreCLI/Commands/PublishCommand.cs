3a52213ad9be6e901a4e6b0d9788aaa5dc9c6a44
Aaron Robinson <aaronrobin1234@gmail.com>
2022-11-14 09:27:32 -0600

    Fix weird incorrect MD5 logic

--- ThunderstoreCLI/Commands/PublishCommand.cs
+++ ThunderstoreCLI/Commands/PublishCommand.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Security.Cryptography;
+using System.Text;
 using ThunderstoreCLI.Configuration;
 using ThunderstoreCLI.Models;
 using ThunderstoreCLI.Utils;
@@ -207,14 +208,41 @@ public static class PublishCommand
             stream.Seek(part.Offset, SeekOrigin.Begin);
 
             byte[] hash;
-            using (var md5 = MD5.Create())
+            var chunk = new MemoryStream();
+            const int blocksize = 65536;
+
+            using (var reader = new BinaryReader(stream, Encoding.Default, true))
             {
-                hash = await md5.ComputeHashAsync(stream);
+                using (var md5 = MD5.Create())
+                {
+                    md5.Initialize();
+                    var length = part.Length;
+
+                    while (length > blocksize)
+                    {
+                        length -= blocksize;
+                        var bytes = reader.ReadBytes(blocksize);
+                        md5.TransformBlock(bytes, 0, blocksize, null, 0);
+                        await chunk.WriteAsync(bytes);
+                    }
+
+                    var finalBytes = reader.ReadBytes(length);
+                    md5.TransformFinalBlock(finalBytes, 0, length);
+
+                    if (md5.Hash is null)
+                    {
+                        Write.ErrorExit($"MD5 hashing failed for part #{part.PartNumber})");
+                        throw new PublishCommandException();
+                    }
+
+                    hash = md5.Hash;
+                    await chunk.WriteAsync(finalBytes);
+                    chunk.Position = 0;
+                }
             }
 
             var request = new HttpRequestMessage(HttpMethod.Put, part.Url);
-            stream.Seek(part.Offset, SeekOrigin.Begin);
-            request.Content = new StreamContent(stream);
+            request.Content = new StreamContent(chunk);
             request.Content.Headers.ContentMD5 = hash;
             request.Content.Headers.ContentLength = part.Length;
 
