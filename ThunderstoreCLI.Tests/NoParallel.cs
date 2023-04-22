987b2c096d2dae9349395f0f6b9fff621e35786a
Ethan Green <ethangreen.dev@gmail.com>
2023-04-22 17:10:58 -0400

    cleanup dotnet source in prep for rust port

--- ThunderstoreCLI.Tests/NoParallel.cs
+++
@@ -1,6 +0,0 @@
-using Xunit;
-
-namespace ThunderstoreCLI.Tests;
-
-[CollectionDefinition(nameof(NoParallel), DisableParallelization = true)]
-public sealed class NoParallel { }
--- ThunderstoreCLI/Defaults.cs
+++
@@ -1,9 +0,0 @@
-namespace ThunderstoreCLI;
-
-public static class Defaults
-{
-    public const string GITHUB_REPO = "thunderstore-cli";
-    public const string GITHUB_USER = "thunderstore-io";
-    public const string PROJECT_CONFIG_PATH = "./thunderstore.toml";
-    public const string REPOSITORY_URL = "https://thunderstore.io";
-}
