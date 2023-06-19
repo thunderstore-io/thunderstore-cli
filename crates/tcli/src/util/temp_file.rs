65c7a91cf9cc1205a8103677173435a50547d7ab
Aaron Robinson <aaronrobin1234@gmail.com>
2023-06-19 18:23:11 -0500

    Remove all unsafe code from TempFile

--- crates/tcli/src/util/temp_file.rs
+++ crates/tcli/src/util/temp_file.rs
@@ -1,21 +1,18 @@
 #![allow(dead_code)]
 
-use std::{
-    mem::ManuallyDrop,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 use crate::error::{Error, IoResultToTcli};
 
-pub struct TempFile<F>(Option<PathBuf>, ManuallyDrop<F>);
+pub struct TempFile<F>(Option<PathBuf>, Option<F>);
 
 impl<F> TempFile<F> {
     pub fn file(&self) -> &F {
-        &self.1
+        self.1.as_ref().unwrap()
     }
 
     pub fn file_mut(&mut self) -> &mut F {
-        &mut self.1
+        self.1.as_mut().unwrap()
     }
 }
 
@@ -28,14 +25,11 @@ impl TempFile<std::fs::File> {
             .write(true)
             .open(path)
             .map_fs_error(path)?;
-        Ok(Self(Some(path.into()), ManuallyDrop::new(file)))
+        Ok(Self(Some(path.into()), Some(file)))
     }
 
     pub fn into_async(mut self) -> TempFile<tokio::fs::File> {
-        TempFile(
-            self.0.take(),
-            ManuallyDrop::new(tokio::fs::File::from_std(self.1.try_clone().unwrap())),
-        )
+        TempFile(self.0.take(), self.1.take().map(tokio::fs::File::from_std))
     }
 }
 
@@ -49,25 +43,18 @@ impl TempFile<tokio::fs::File> {
             .open(path)
             .await
             .map_fs_error(path)?;
-        Ok(Self(Some(path.into()), ManuallyDrop::new(file)))
+        Ok(Self(Some(path.into()), Some(file)))
     }
 
     pub async fn into_std(mut self) -> TempFile<std::fs::File> {
-        // get the new instance, drop the first, then convert to std (if done like to_async, this would panic)
-        let path = self.0.take();
-
-        let new_async = self.1.try_clone().await.unwrap();
-
-        drop(self);
-
-        TempFile(path, ManuallyDrop::new(new_async.into_std().await))
+        TempFile(self.0.take(), Some(self.1.take().unwrap().into_std().await))
     }
 }
 
 impl<F> Drop for TempFile<F> {
     fn drop(&mut self) {
-        // SAFETY: This is in the drop implementation, we just need this to be dropped before we delete
-        unsafe { ManuallyDrop::drop(&mut self.1) }
+        // We need this file handle to be dropped before we delete
+        drop(self.1.take());
         if let Some(path) = &self.0 {
             std::fs::remove_file(path).expect("Failed to remove temporary file.");
         }
