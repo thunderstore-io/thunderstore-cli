1a5a9ee0ace4383475cfa6d7ed532da8525e5fa8
Ethan Green <ethangreen.dev@gmail.com>
2023-10-17 11:42:39 -0400

    add matrix field to InstallerManifest
    This field allows for executable paths to be specified for an arbitrary combination of OS and architecture targets.

--- crates/tcli/src/package/install/manifest.rs
+++ crates/tcli/src/package/install/manifest.rs
@@ -1,13 +1,19 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use crate::util::os::OS;
+use crate::util::os::{ARCH, OS};
 
 /// This manifest exists alongside the typical Thunderstore package manifest.
 /// It contains additional information about the type of installer, the runner that will be used,
 /// dependencies, etc.
 #[derive(Serialize, Deserialize)]
 pub struct InstallerManifest {
-    pub executable: String,
     pub installer_version: u32,
+    pub matrix: Vec<InstallerMatrix>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct InstallerMatrix {
     pub target_os: OS,
+    pub architecture: ARCH,
+    pub executable: String,
 }
--- crates/tcli/src/util/os.rs
+++ crates/tcli/src/util/os.rs
@@ -1,11 +1,21 @@
-use serde::{Serialize, Deserialize};
 use std::fmt::{Display, Formatter};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
 pub enum OS {
     Windows,
     MacOS,
     Linux,
+}
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+
+pub enum ARCH {
+    X86_64,
+    X86,
+    AArch64,
+    Arm,
 }
 
 impl Display for OS {
@@ -29,6 +39,33 @@ impl From<String> for OS {
             "macos" => OS::MacOS,
             "linux" => OS::Linux,
             _ => panic!("'{value}' is not a valid OS name."),
+        }
+    }
+}
+
+impl Display for ARCH {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let str_name = match self {
+            ARCH::X86_64 => "x86_64",
+            ARCH::X86 => "x86",
+            ARCH::AArch64 => "aarch64",
+            ARCH::Arm => "arm",
+        };
+
+        write!(f, "{str_name}")
+    }
+}
+
+impl From<String> for ARCH {
+    fn from(value: String) -> Self {
+        let lowercase = value.to_lowercase();
+
+        match lowercase.as_str() {
+            "x86_64" => ARCH::X86_64,
+            "x86" => ARCH::X86,
+            "aarch64" => ARCH::AArch64,
+            "arm" => ARCH::Arm,
+            _ => panic!("'{value}' is not a valid architecture name."),
         }
     }
 }
