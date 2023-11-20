0d7abd3941c68916876d1c3308186fcdee1716d9
Ethan Green <ethangreen.dev@gmail.com>
2023-11-20 13:43:27 -0500

    add `DependencyGraph` struct with implementation
    The DependencyGraph struct wraps code that was previously contained entirely within the resolve_package function. It allows for the dependency graph of one or more packages to be queried and analyzed.
    
    The dependency graph itself is an amalgamation of a directed graph (DAG) and an index. The index allows for nodes within the graph to be found with their loose identifier strings, and the graph itself contains nodes and edges. The order in which a set of packages should be installed is determined with a topological sort, and child dependencies of a given node can be found with dijkstra's traversal algorithm.

--- crates/tcli/src/package/install/legacy_compat.rs
+++ crates/tcli/src/package/install/legacy_compat.rs
@@ -1,5 +1,24 @@
-use crate::package::Package;
+use std::str::FromStr;
 
-pub fn determine_installer_from_contents(package: &Package) {
+use crate::package::Package;
+use crate::ts::package_reference::PackageReference;
+
+/// Attempt to determine the valid installer for the given package by searching through
+/// the resolved package dependency list.
+///
+/// Note: This is not a "correct" solution as it does not explicitly search for *dependencies*
+/// of the given package. This works well enough though, given that we don't support multi
+/// mod loader installs.
+pub fn determine_installer_from_resolved(packages: &[PackageReference]) -> Option<PackageReference> {    
+    for package in packages {
+        if package.name.to_lowercase() == "bepinex" {
+            return Some(PackageReference::from_str("0x7FF-BepInExInstaller-0.0.1").unwrap())
+        }
+    }
+
+    None
+}
+
+pub fn determine_installer_from_contents(package: &Package) -> Option<PackageReference> {
     todo!()
 }
