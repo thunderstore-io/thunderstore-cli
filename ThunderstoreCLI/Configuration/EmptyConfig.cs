577b6af477e5e4a2cbf91b34cbfc27d26d1bac4c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-14 18:42:35 +0000

    to expression-bodied properties

--- ThunderstoreCLI/Configuration/EmptyConfig.cs
+++ ThunderstoreCLI/Configuration/EmptyConfig.cs
@@ -4,53 +4,23 @@ namespace ThunderstoreCLI.Configuration;
 public abstract class EmptyConfig : IConfigProvider
 {
     public virtual void Parse(Config currentConfig) { }
-    public virtual GeneralConfig? GetGeneralConfig()
-    {
-        return null;
-    }
+    public virtual GeneralConfig? GetGeneralConfig() => null;
 
-    public virtual PackageConfig? GetPackageMeta()
-    {
-        return null;
-    }
+    public virtual PackageConfig? GetPackageMeta() => null;
 
-    public virtual InitConfig? GetInitConfig()
-    {
-        return null;
-    }
+    public virtual InitConfig? GetInitConfig() => null;
 
-    public virtual BuildConfig? GetBuildConfig()
-    {
-        return null;
-    }
+    public virtual BuildConfig? GetBuildConfig() => null;
 
-    public virtual PublishConfig? GetPublishConfig()
-    {
-        return null;
-    }
+    public virtual PublishConfig? GetPublishConfig() => null;
 
-    public virtual InstallConfig? GetInstallConfig()
-    {
-        return null;
-    }
+    public virtual InstallConfig? GetInstallConfig() => null;
 
-    public virtual AuthConfig? GetAuthConfig()
-    {
-        return null;
-    }
+    public virtual AuthConfig? GetAuthConfig() => null;
 
-    public virtual ModManagementConfig? GetModManagementConfig()
-    {
-        return null;
-    }
+    public virtual ModManagementConfig? GetModManagementConfig() => null;
 
-    public virtual GameImportConfig? GetGameImportConfig()
-    {
-        return null;
-    }
+    public virtual GameImportConfig? GetGameImportConfig() => null;
 
-    public virtual RunGameConfig? GetRunGameConfig()
-    {
-        return null;
-    }
+    public virtual RunGameConfig? GetRunGameConfig() => null;
 }
